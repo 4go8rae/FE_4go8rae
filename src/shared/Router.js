9b2c7b81ae72d64e649db40a8685f0ceee5d3f5f
Yunjae <nuguri206@gmail.com>
2022-11-03 22:41:18 +0900

    feat: 상품 등록 페이지 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Chat from "../pages/Chat";
 import Main from "../pages/Main";
+import Post from "../pages/Post";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/form" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
