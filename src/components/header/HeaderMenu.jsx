6199352c9f845df40771563014fffd1ab21fe396
Yunjae <nuguri206@gmail.com>
2022-11-03 22:39:57 +0900

    <feat> : header 컴포넌트 추가

---
+++ src/components/header/HeaderMenu.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Swal from "sweetalert2";
+
+const HeaderMenu = ({ modalHandler }) => {
+  const navigate = useNavigate();
+
+  // 로그인 여부와 관리자 여부를 확인하기 위해 storage에서 가져온 데이터
+  const role = sessionStorage.getItem("role");
+  const token = sessionStorage.getItem("ACCESS_TOKEN");
+
+  const modalRef = useRef();
+
+  useEffect(() => {
+    document.addEventListener("mousedown", clickModalOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", clickModalOutside);
+    };
+  });
+
+  const clickModalOutside = (event) => {
+    if (!modalRef.current.contains(event.target)) {
+      modalHandler();
+    }
+  };
+
+  return (
+    <MenuContainer>
+      <Menu ref={modalRef}>
+        <h3 onClick={() => navigate("/")}>홈</h3>
+        <br />
+        {token !== null ? (
+          <>
+            <h3 onClick={() => navigate("/mypage")}>마이페이지</h3>
+            <h3 onClick={() => navigate("/form")}>상품등록</h3>
+          </>
+        ) : null}
+        <div>
+          {token === null ? (
+            <h3 onClick={() => navigate("/login")}>로그인 ＞</h3>
+          ) : (
+            <h3
+              onClick={() => {
+                // logout();
+              }}
+            >
+              로그아웃 ＞
+            </h3>
+          )}
+        </div>
+      </Menu>
+    </MenuContainer>
+  );
+};
+
+export default HeaderMenu;
+
+const MenuContainer = styled.div`
+  margin: 0 auto;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 999;
+`;
+
+const Menu = styled.div`
+  position: absolute;
+  left: 52.1%;
+  max-width: 214px;
+  width: 50%;
+  float: right;
+  height: 100vh;
+  top: calc(0vh + 70px);
+  background-color: #abd4e2;
+  text-align: center;
+  color: #535353;
+  transition: all 0.3s;
+  z-index: 999;
+  margin-top: -1px;
+
+  & h3 {
+    margin: 30px auto;
+
+    &:hover {
+      cursor: pointer;
+      font-weight: bold;
+    }
+  }
+
+  & div {
+    margin-top: 60px;
+    color: white;
+  }
+`;
