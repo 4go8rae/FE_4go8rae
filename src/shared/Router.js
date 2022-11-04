5179d4a260975926d7efc9fa3e555d3a20b12e17
Yunjae <nuguri206@gmail.com>
2022-11-04 12:39:06 +0900

    feat: 디테일페이지 컴포넌트 생성

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Chat from "../pages/Chat";
+import Detail from "../pages/Detail";
 import Main from "../pages/Main";
 import Post from "../pages/Post";
 
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/form" element={<Post />} />
+        <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
